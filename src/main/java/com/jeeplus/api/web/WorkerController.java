ebece76c65f76b03d62d8aa9a8d31bebbbaf9953
yzliud <654442366@qq.com>
2017-04-18 14:20:17 +0800

    进度上传

--- src/main/java/com/jeeplus/api/web/WorkerController.java
+++ src/main/java/com/jeeplus/api/web/WorkerController.java
@@ -17,9 +17,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.jeeplus.api.service.ContractScheduleService;
 import com.jeeplus.api.service.ContractService;
 import com.jeeplus.api.util.Sms;
@@ -249,7 +249,43 @@ public class WorkerController extends BaseController {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter writer = response.getWriter();
 
-		String uuid = request.getParameter("uuid");
+		//创建一个通用的多部分解析器  
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());  
+        //判断 request 是否有文件上传,即多部分请求  
+        if(multipartResolver.isMultipart(request)){  
+        	MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;  
+            //取得request中的所有文件名  
+        	List<MultipartFile> files = multiRequest.getFiles("uploaderInput");  
+            for(MultipartFile file:files ){  
+                //记录上传过程起始时的时间，用来计算上传时间  
+                int pre = (int) System.currentTimeMillis();  
+                //取得上传文件  
+                if(file != null){  
+                    //取得当前上传文件的文件名称  
+                    String myFileName = file.getOriginalFilename();  
+                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在  
+                    if(myFileName.trim() !=""){  
+                        System.out.println(myFileName);  
+                        //重命名上传后的文件名  
+                        String fileName = "demoUpload" + file.getOriginalFilename();  
+                        //定义上传路径  
+                        String path = "e:/" + fileName;  
+                        File localFile = new File(path);  
+                        file.transferTo(localFile);  
+                    }  
+                }  
+                //记录上传该文件后的时间  
+                int finaltime = (int) System.currentTimeMillis();  
+                System.out.println(finaltime - pre);  
+            }
+        }
+		
+		writer.println("1");
+		writer.flush();
+		writer.close();
+
+
+		/*String uuid = request.getParameter("uuid");
 		String uploadPath = request.getSession().getServletContext().getRealPath("/upload");
 		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
 
@@ -278,7 +314,7 @@ public class WorkerController extends BaseController {
 		map.put("imgname", nwename);
 		writer.println(gson.toJson(map));
 		writer.flush();
-		writer.close();
+		writer.close();*/
 	}
 
 	private String saveImg(MultipartFile file, String uuid, String uploadPath, String code)
