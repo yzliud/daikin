d2645d81c172b9a5a3405ff03afde8e2cc46d977
xqn <xqn@DESKTOP-4BERPUI>
2017-05-30 10:02:11 +0800

    1

--- src/main/java/com/jeeplus/api/service/QuotationService.java
+++ src/main/java/com/jeeplus/api/service/QuotationService.java
@@ -1,14 +1,16 @@
 package com.jeeplus.api.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jeeplus.common.service.CrudService;
 import com.jeeplus.modules.daikin.dao.DkQuotationDao;
 import com.jeeplus.modules.daikin.entity.DkQuotation;
 
+@Service
+@Transactional(readOnly = true)
 public class QuotationService  extends CrudService<DkQuotationDao, DkQuotation>{
 
-	@Transactional(readOnly = false)
 	public void save(DkQuotation dkQuotation,String product) {
 				
 	}
